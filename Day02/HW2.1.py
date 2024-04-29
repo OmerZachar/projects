541557f46b6164afcc5f23db51e13f91aa3ac251
Omer Zachar <omerzachar@Omers-Air.lan>
2024-04-30 00:05:19 +0300

    update

---
+++ Day02/HW2.1.py
@@ -0,0 +1,16 @@
+select = random.randint(1, 20)
+user_input = int(input("Guess a number between 1 and 20: "))
+counter = 1
+
+while True:
+    if user_input < select:
+        print('The number is too small, guess again')
+    elif user_input > select:
+        print('The number is too big, guess again')
+    else:
+        break 
+
+    counter += 1
+    user_input = int(input("Guess a number between 1 and 20: "))
+
+print(f"Congratulations! You answered correctly after {counter} times.")
