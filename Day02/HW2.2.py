038ff04252a06acebec9ce1c990427c4a206a049
Omer Zachar <omerzachar@Omers-MacBook-Air.local>
2024-04-30 18:36:47 +0300

    update

---
+++ Day02/HW2.2.py
@@ -0,0 +1,16 @@
+import random
+
+computer = random.randint(1,20)
+turns = 0
+
+while True:
+    turns += 1
+    user = int(input("Guess a number between 1 and 20: "))
+    if user == computer:
+        print(f"Good guess!")
+        break
+    elif user < computer:
+        print(f"Your guess is too small, try again")
+    else:
+        print(f"Your guess is too big, try again")
+print(f"You found the correct number after {turns} rounds")
