bc1b97649b5a4690e025e81ea0c41dc2ea8a3f22
Omer Zachar <omerzachar@Omers-Air.lan>
2024-05-05 18:15:14 +0300

    update

--- Day02/HW2.2.py
+++ Day02/HW2.2.py
@@ -6,11 +6,21 @@ turns = 0
 while True:
     turns += 1
     user = int(input("Guess a number between 1 and 20: "))
-    if user == computer:
-        print(f"Good guess!")
+    if user == 'x':
+        print('Game over')
         break
-    elif user < computer:
-        print(f"Your guess is too small, try again")
-    else:
-        print(f"Your guess is too big, try again")
-print(f"You found the correct number after {turns} rounds")
+    elif user == 'n':
+        print('Game over, Do u want to restart?')
+        break
+    elif user == 's':
+        print(f"The number was {computer}")
+        break
+    else: 
+        if user == computer:
+            print(f"Good guess!")
+            break
+        elif user < computer:
+            print(f"Your guess is too small, try again")
+        else:
+            print(f"Your guess is too big, try again")
+            print(f"You found the correct number after {turns} rounds")
